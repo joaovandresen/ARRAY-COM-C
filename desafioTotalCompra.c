5fc4c417607af40b73fb1f6627a7d31cbdf3fe02
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-10-09 14:51:20 +0000

    finalizado

---
+++ desafioTotalCompra.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    char produto[50]; // Guardar o nome do produto
+    float preco;     // Preço Unitário
+    int quantidade; // Quantidade comprada
+    float total;   //  Valor total da compra
+
+    printf("Digite o nome do produto: ");
+    scanf("%s",produto);
+
+    printf("Digite o preço unitário: ");
+    scanf("%f",&preco);
+
+    printf("Digite a quantidade: ");
+    scanf("%d",&quantidade);
+
+    total = preco * quantidade;
+
+    printf("Produto: %s\n,produto");
+    printf("Preço Unitário: %.2f\n", preco);
+    printf("Quantidade: %d\n", quantidade);
+    printf("Total da compra: %.2f\n",total);
+    return 0;
+}
